a64d8e97816e2aebf620d7398e55e62889f4845c
Danilo <danilofreirea90@gmail.com>
2025-05-15 15:03:23 -0300

    Componente Carrinho #1

--- lib/views/cart_view.dart
+++ lib/views/cart_view.dart
@@ -1,15 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:minha_loja/models/cart.dart';
+import 'package:provider/provider.dart';
 
 class CartView extends StatelessWidget {
   const CartView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final Cart cart = Provider.of<Cart>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Carrinho'),
         backgroundColor: Theme.of(context).primaryColor,
         foregroundColor: Colors.white,
+      ),
+      body: Column(
+        children: [
+          Card(
+            margin: const EdgeInsets.all(25),
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Total', style: TextStyle(fontSize: 20)),
+                  SizedBox(width: 20),
+
+                  Text(
+                    'R\$${cart.totalAmount.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Theme.of(context).primaryColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  // Chip(
+                  //   labelPadding: const EdgeInsets.all(4),
+                  //   backgroundColor: Theme.of(context).primaryColor,
+                  //   label: Text(
+                  //     'R\$1000',
+                  //     style: TextStyle(
+                  //       color:
+                  //           Theme.of(
+                  //             context,
+                  //           ).primaryTextTheme.headlineLarge?.color,
+                  //     ),
+                  //   ),
+                  // ),
+                  Spacer(),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text('COMPRAR'),
+                    style: TextButton.styleFrom(
+                      backgroundColor: Theme.of(context).primaryColor,
+                      foregroundColor:
+                          Theme.of(
+                            context,
+                          ).primaryTextTheme.headlineLarge?.color,
+                      textStyle: TextStyle(
+                        color:
+                            Theme.of(
+                              context,
+                            ).primaryTextTheme.headlineLarge?.color,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
