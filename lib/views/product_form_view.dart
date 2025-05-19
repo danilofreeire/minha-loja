d728b72c9fcd5bb6fd825e9f3dd32accab067b71
Danilo <danilofreirea90@gmail.com>
2025-05-19 12:31:30 -0300

    Campo de Entrada de URL

--- lib/views/product_form_view.dart
+++ lib/views/product_form_view.dart
@@ -8,18 +8,34 @@ class ProductFormView extends StatefulWidget {
 }
 
 class _ProductFormViewState extends State<ProductFormView> {
+  final _priceFocus = FocusNode();
+  final _descriptionFocus = FocusNode();
+
+  final _imageUrlFocus = FocusNode();
+  final _imageUrlController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _imageUrlFocus.addListener(updateImage);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _priceFocus.dispose();
+    _descriptionFocus.dispose();
+
+    _imageUrlFocus.removeListener(updateImage);
+    _imageUrlFocus.dispose();
+  }
+
+  void updateImage() {
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    final _priceFocus = FocusNode();
-    final _descriptionFocus = FocusNode();
-
-    @override
-    void dispose() {
-      super.dispose();
-      _priceFocus.dispose();
-      _descriptionFocus.dispose();
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Formulário de Produto'),
@@ -54,6 +70,39 @@ class _ProductFormViewState extends State<ProductFormView> {
                 focusNode: _descriptionFocus,
                 keyboardType: TextInputType.multiline,
                 maxLines: 3,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'URL da Imagem'),
+                      textInputAction: TextInputAction.done,
+                      focusNode: _imageUrlFocus,
+                      keyboardType: TextInputType.url,
+                      controller: _imageUrlController,
+                    ),
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: const EdgeInsets.only(top: 10, left: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey, width: 1),
+                    ),
+                    alignment: Alignment.center,
+                    child:
+                        _imageUrlController.text.isEmpty
+                            ? Text('Preview')
+                            : FittedBox(
+                              child: Image.network(
+                                _imageUrlController.text,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                  ),
+                ],
               ),
             ],
           ),
